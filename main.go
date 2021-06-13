1690d6fa30c9db57c5685168b60080326f97c0f8
小野和論 <matopenkw@gmail.com>
2021-06-13 21:52:53 +0900

    fix main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,9 +11,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
+func hostname(w http.ResponseWriter, r *http.Request) {
+	hn := os.Getenv("HOSTNAME")
+	fmt.Println(hn)
+	fmt.Fprintf(w, hn)
+}
+
 func main() {
 	fmt.Println("Start Server")
 
-	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/hostname", hostname)
 	http.ListenAndServe(":8080", nil)
 }
